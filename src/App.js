43dcb0ecedd062d0948ac6b8cd253d9df59f12fa
MarryGennad <maria-gennadievna@outlook.com>
2024-08-11 09:30:27 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useState } from "react";
 
-function App() {
+const MoneyConverter = () => {
+  const [money, setMoney] = useState("");
+
+  const handleInput = (e) => {
+    const rubles = e.target.value;
+    setMoney(rubles);
+  };
+
+  const changeMoney = () => {
+    const usd = (money * 87.99).toFixed(2);
+    const eur = (money * 95.18).toFixed(2);
+    const cny = (money * 11.89).toFixed(2);
+    return { usd, eur, cny };
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1>¯\_(ツ)_/¯</h1>
+        <h2>Money Converter</h2>
+        <div className="wrapper">
+          <input
+            type="number"
+            onChange={handleInput}
+            placeholder="enter rubles"
+          ></input>
+          <div className="money usd">
+            <div>{changeMoney().usd}</div>
+            usd
+          </div>
+          <div className="money eur">
+            <div>{changeMoney().eur}</div>
+            euro
+          </div>
+          <div className="money cny">
+            <div>{changeMoney().cny}</div>
+            cny
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
-
-export default App;
+};
+export default MoneyConverter;
